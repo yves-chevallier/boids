0a284ed447eabd57270cebb9e605e4c0b8bdc9bd
Yves Chevallier <yves.chevallier@heig-vd.ch>
2021-11-26 22:17:23 +0100

    Add 2D vector

---
+++ vector.cpp
@@ -0,0 +1,157 @@
+/**
+ * 2D Vector in a vector space.
+ */
+#include "vector.hpp"
+
+#include <cmath>
+
+Vector::Vector() : x{0}, y{0} {}
+Vector::Vector(double x, double y) : x{x}, y{y} {}
+
+Vector Vector::operator+(const double scalar)
+{
+    return Vector(x * scalar, y * scalar);
+}
+
+Vector Vector::operator-(const double scalar)
+{
+    return Vector(x - scalar, y - scalar);
+}
+
+Vector Vector::operator*(const double scalar)
+{
+    return Vector(x * scalar, y * scalar);
+}
+
+Vector Vector::operator/(const double scalar)
+{
+    return Vector(x / scalar, y / scalar);
+}
+
+Vector Vector::operator+(const Vector &other)
+{
+    return Vector(x + other.x, y + other.y);
+}
+
+Vector Vector::operator-(const Vector &other)
+{
+    return Vector(x - other.x, y - other.y);
+}
+
+Vector Vector::operator*(const Vector &other)
+{
+    return Vector(x * other.x, y * other.y);
+}
+
+Vector Vector::operator/(const Vector &other)
+{
+    return Vector(x / other.x, y / other.y);
+}
+
+Vector &Vector::operator*=(double scalar)
+{
+    x *= scalar;
+    y *= scalar;
+    return *this;
+}
+
+Vector &Vector::operator/=(double scalar)
+{
+    x /= scalar;
+    y /= scalar;
+    return *this;
+}
+
+Vector &Vector::operator+=(double scalar)
+{
+    x += scalar;
+    y += scalar;
+    return *this;
+}
+
+Vector &Vector::operator+=(Vector Vector)
+{
+    x += Vector.x;
+    y += Vector.y;
+    return *this;
+}
+
+double Vector::norm() const
+{
+    return sqrt(x * x + y * y);
+}
+
+Vector &Vector::normalize()
+{
+    float magnitude = norm();
+    if (magnitude != 0)
+        *this /= magnitude;
+    return *this;
+}
+
+Vector &Vector::limit(double max)
+{
+    float magnitude = norm();
+    if (magnitude > max)
+        *this *= max / magnitude;
+    return *this;
+}
+
+void Vector::fmod(double mod)
+{
+    ::fmod(x, mod);
+    ::fmod(y, mod);
+}
+
+double Vector::angle() const
+{
+    return ::atan2(y, x);
+}
+
+double Vector::distance(Vector other) const
+{
+    double dx = x - other.x;
+    double dy = y - other.y;
+    return sqrt(dx * dx + dy * dy);
+}
+
+/**
+ * Toroidal distance in a Wrap Around space.
+ * @param other Other vector point
+ * @param width Width of the vector space
+ * @param height Height of the vector space
+ */
+double Vector::toroidal_distance2(const Vector &other, double width, double height) const
+{
+    double dx = x - other.x;
+    double dy = y - other.y;
+
+    dx = dx > width / 2 ? width - dx : dx;
+    dy = dy > height / 2 ? height - dy : dy;
+
+    return dx * dx + dy * dy;
+}
+
+double Vector::toroidal_distance(const Vector &other, double width, double height) const
+{
+    return sqrt(toroidal_distance2(other, width, height));
+}
+
+double Vector::angle(const Vector &other) const
+{
+    return ::atan2(other.y - y, other.x - x);
+}
+
+static double frand() { return (double)rand() / RAND_MAX; }
+
+Vector::operator std::string() const
+{
+    char buff[64];
+    snprintf(buff, sizeof(buff), "(%.2f, %.2f)", x, y);
+    return std::string(buff);
+}
+
+Vector Vector::random(double max, double offset)
+{
+    return Vector(frand() * max + offset, frand() * max + offset);
+}
---
+++ vector.hpp
@@ -0,0 +1,46 @@
+/**
+ * 2D Vector in a vector space.
+ */
+#pragma once
+#include <iostream>
+
+class Vector
+{
+public:
+    double x;
+    double y;
+
+    Vector();
+    Vector(double x, double y);
+
+    Vector operator+(const double scalar);
+    Vector operator-(const double scalar);
+    Vector operator*(const double scalar);
+    Vector operator/(const double scalar);
+
+    Vector operator+(const Vector &vector);
+    Vector operator-(const Vector &vector);
+    Vector operator*(const Vector &vector);
+    Vector operator/(const Vector &vector);
+
+    Vector &operator*=(double scalar);
+    Vector &operator/=(double scalar);
+    Vector &operator+=(double scalar);
+    Vector &operator+=(Vector Vector);
+
+    void fmod(double mod);
+
+    Vector &normalize();
+    Vector &limit(double max = 1.0);
+
+    double distance(Vector other) const;
+    double toroidal_distance(const Vector &other, double width = 1.0, double height = 1.0) const;
+    double toroidal_distance2(const Vector &other, double width = 1.0, double height = 1.0) const;
+    double norm() const;
+    double angle() const;
+    double angle(const Vector &other) const;
+
+    operator std::string() const;
+
+    static Vector random(double max = 1.0, double offset = 0.0);
+};
